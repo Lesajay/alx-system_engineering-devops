9d0c0bc8bccdcfd19db1db89db04615fd26754cf
Lesajay <craigmazwi@gmail.com>
2023-05-10 19:13:25 +0000

    first

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,10 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux i686 on x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2650.46 Safari/537.36"
+        "User-Agent":
+        "Mozilla/5.0 (X11; Ubuntu; Linux i686 on x86_64) \
+        AppleWebKit/537.36 (KHTML, like Gecko) \
+        Chrome/49.0.2650.46 Safari/537.36"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
