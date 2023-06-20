a846333824201f036a4519f276697275b59e8cf0
Lesajay <craigmazwi@gmail.com>
2023-06-20 16:02:21 +0000

    corrections

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,11 @@
 # Fix the nginx requests limit
-exec { 'upgrade':
-  path    => '/bin/',
-  command => 'sed -i "s/15/4096" /etc/default/nginx',
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-exec { 'restart':
-  path    => '/usr/bin/',
-  command => 'service nginx restart',
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
